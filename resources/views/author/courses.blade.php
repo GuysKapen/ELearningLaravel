f2c7527688045b9ec54cebbfeed916e8ab18d6e8
GuysKapen <guyskapen@gmail.com>
2022-04-26 15:53:07 -0800

    Add instructor layout and link pages together

--- resources/views/author/courses.blade.php
+++ resources/views/author/courses.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.frontend.app')
+@extends('layouts.instructor.app')
 
 @section('title', 'Courses')
 
---
+++ resources/views/layouts/instructor/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('/favicon.png') }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    @stack('css')
+</head>
+<body class="theme-blue">
+<!-- Top Bar -->
+@include('layouts.instructor.partial.topbar')
+<!-- #Top Bar -->
+<div class="flex">
+    <section>
+        <!-- Left Sidebar -->
+    @include('layouts.instructor.partial.sidebar')
+    <!-- #END# Left Sidebar -->
+    </section>
+
+    <section class="content flex-grow">
+        @yield('content')
+    </section>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+{!! Toastr::message() !!}
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+@stack('js')
+</body>
+</html>
