9dea4139f97af0cfe2b9fe96022bab31e376071e
GuysKapen <guyskapen@gmail.com>
2022-10-07 14:29:48 +0700

    Add basic seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Add roles;
+        // Admin id is 1
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'slug' => Str::slug('admin'),
+        ]);
+
+        // Author id is 2
+        DB::table('roles')->insert([
+            'id' => 2,
+            'name' => 'author',
+            'slug' => Str::slug('author'),
+        ]);
+
+        // User is 3
+        DB::table('roles')->insert([
+            'id' => 3,
+            'name' => 'user',
+            'slug' => Str::slug('user'),
+        ]);
+
+        // Admin
+        User::factory()->state([
+            'role_id' => '1',
+            'email' => 'admin@gmail.com'
+        ])->create();
+
+        // Author
+        User::factory()->state([
+            'role_id' => '2',
+            'email' => 'author@gmail.com'
+        ])->create();
     }
 }
